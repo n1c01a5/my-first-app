96454e99c038087cb67165e01c3b24713c6f15e7
Wagner Nicolas <contact@wagner-nicolas.com>
2021-09-22 16:03:28 +0200

    feat: init commit

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,6 +1,6 @@
 const { createLambdaFunction, createProbot } = require('@probot/adapter-aws-lambda-serverless');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 module.exports.handler = createLambdaFunction(app, {
   probot: createProbot(),
